8b2660e7789ccf9c2ce916d9ff9302c73b8a40a5
oliverlai55 <oliverlai55@gmail.com>
2016-10-22 16:50:31 -0400

    Complete select book feature

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,5 @@
 //state argument is NOT application state, only the state this reducer is responsible for
-export default function(state, action) {
+export default function(state = null, action) {
   switch(action.type) {
   case 'BOOK_SELECTED':
     return action.payload;
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return (
-    { title: 'Book1'},
-    { title: 'Book2'},
-    { title: 'Book3'},
-    { title: 'Book4'}
+    { title: 'Book1', pages: 101},
+    { title: 'Book2', pages: 39},
+    { title: 'Book3', pages: 85},
+    { title: 'Book4', pages: 1}
   )
 }
 
